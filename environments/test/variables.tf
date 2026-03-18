265475cec1f0276e3cb4453a3eb39ca532dc9eb7
Y-Ibrahim <yibro.i2001@gmail.com>
2026-03-18 13:34:45 +0000

    Included new variables

--- environments/test/variables.tf
+++ environments/test/variables.tf
@@ -3,8 +3,23 @@ variable "cluster_name" {
     default = "test"
 }
 
-variable "region" {
+variable "location" {
     description = "region for cluster"
-    default = "UKSouth"
+    default = "uksouth"
   
+}
+
+variable "subscription_id" {
+  description = "The subscription ID for the Azure account"
+  type        = string
+}
+
+variable "node_count" {
+    description = "The no. of nodes to create"
+    type = string
+    default = 2
+}
+
+variable "ssh_key" {
+    description = "ssh public key associated with VM"
 }
