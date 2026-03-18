e032644254ed17e7c010aedc1d3dea20deeb2323
Y-Ibrahim <yibro.i2001@gmail.com>
2026-03-18 13:38:27 +0000

    Created new parameters for AKS cluster

--- modules/tf_module_full_cluster/main.tf
+++ modules/tf_module_full_cluster/main.tf
@@ -1,31 +1,52 @@
-# Create ACR
-resource "azurerm_container_registry" "acr" {
-  name = var.acr_name
-  resource_group_name = var.resource
-  location = var.location
-  admin_enabled = false
-}
-
 # Create AKS cluster configuration
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                         = "${var.name}-cluster"
-  resource_group_name          = var.resource
-  location                     = "${var.region}"
-  agent_public_key_certificate = filebase64("testdata/public.cer")
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
   dns_prefix                   = var.name
 
   default_node_pool {
     name = "system"
-    node_count = 2
+    node_count = var.node_count
     vm_size = var.vm_size
   }
+
+  tags = {
+    ENV = var.name
+  }
+
   identity {
     type = "SystemAssigned"
   }
 
-  tags = {
-    ENV = "Test"
+  linux_profile {
+    admin_username = "azureuser"
+    ssh_key {
+      key_data = var.ssh_key
+    }
   }
+
+  network_profile {
+    network_plugin = "kubenet"
+    load_balancer_sku = "standard"
+  }
+
+  azure_active_directory_role_based_access_control {
+  azure_rbac_enabled     = true
+  admin_group_object_ids = [var.admin_group_object_id]
+  }
+
 }
 
-# Configure 
+
+resource "azuread_group" "aks_devops" {
+  display_name     = "AKS-DevOps"
+  security_enabled = true
+  description      = "DevOps access to AKS cluster"
+}
+
+resource "azurerm_role_assignment" "aks_admin" {
+  scope                = azurerm_kubernetes_cluster.aks_cluster.id
+  role_definition_name = "Azure Kubernetes Service RBAC Cluster Admin"
+  principal_id         = var.admin_group_object_id
+}
--- modules/tf_module_full_cluster/variables.tf
+++ modules/tf_module_full_cluster/variables.tf
@@ -1,3 +1,7 @@
+variable "ssh_key" {
+    description = "ssh public key associated with VM"
+    type = string
+}
 variable "name" {
   description = "name of cluster"
 }
@@ -6,20 +10,32 @@ variable "resource_group_name" {
     description = "name of resource group" 
 }
 
-variable "location"{
+variable "location" {
     description = "location of resources"
-}
-
-variable "dns_prefix" {
-    description = "value"
+    type = string
+    default = "uksouth"
 }
 
 variable "vm_size" {
     description = "size of vm"
+    type = string
     default = "Standard_B4ms"
 }
 
 variable "acr_name" {
     description = "name of azure container registry"
+    type = string
     default = "portfolio-website"
+}
+
+variable "node_count" {
+    type = string
+    description = "no. of nodes to create"
+    default = 2
+
+}
+
+variable "admin_group_object_id" {
+    type = string
+    description = "ID of admin group object"
 }
